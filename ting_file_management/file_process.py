1ac109cce702c9a331ba6ebdd45b3b4235e87496
Felipe Pinto <felipe14062000@gmail.com>
2023-06-06 17:50:59 -0300

    feat: implementação das funções is_alredy_in_process, process, remove, file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,41 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def is_alredy_in_process(path_file, instance):
+    files = [
+        instance.search(index)["nome_do_arquivo"]
+        for index in range(len(instance))
+    ]
+    return path_file in files
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if is_alredy_in_process(path_file, instance):
+        return
+    else:
+        lines = txt_importer(path_file)
+        info_file = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(info_file)
+        print(info_file, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+    else:
+        path_file = instance.dequeue()["nome_do_arquivo"]
+        print(f"Arquivo {path_file} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        content = instance.search(position)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
+    else:
+        print(content, file=sys.stdout)
