69200883c47dd9f62b78b482555b0f08c6d571f4
Felipe Pinto <felipe14062000@gmail.com>
2023-06-06 21:17:32 -0300

    feat: implementação das funções de busca por palavra exists_word e search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,48 @@
+def search_word(lines, word, type):
+    result_list = {"complete": list(), "simples": list()}
+
+    for line in lines:
+        if word.lower() in line.lower():
+            if type == "complete":
+                result_list["complete"].append(
+                    {"linha": lines.index(line) + 1, "conteudo": line}
+                )
+            else:
+                result_list["simples"].append({"linha": lines.index(line) + 1})
+    return result_list[type]
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = list()
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = search_word(
+            file["linhas_do_arquivo"], word, type="simples"
+        )
+        if len(occurrences) > 0:
+            search_result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return search_result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = list()
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = search_word(
+            file["linhas_do_arquivo"], word, type="complete"
+        )
+        if len(occurrences) > 0:
+            search_result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return search_result
